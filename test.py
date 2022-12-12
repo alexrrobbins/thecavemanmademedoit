d8cc0909e0a3f4c260a0519238c695d89a2a28a1
Alex Robbins <alexrrobbins@gmail.com>
2022-12-12 15:37:34 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+dummy = '1'
+quote = "2"
+
+print('Hello gecko!')
+
+testing = "geico\a123456"
+
+for c in testing:
+	print(c)
+	dummy = dummy + quote
+
+print("Connecting to fun.geico.net")
+
+tested = 1 + 6 / 3
